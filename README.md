3faf450cc507b4ad485deaa19a25cd4896125c7b
Matan Brightbert <matana0000@gmail.com>
2022-12-22 01:41:03 +0200

    Rewrited code and updated 'README.md'

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
-# PowerShell-Windows11-SetDarkModeAndNightLightStates
+# PowerShell-Windows-DarkModeScripts
 
-This PowerShell script sets the dark-mode's and night-light's states of the Windows OS according to the given times. It can be used in a scheduled task.\
-Please note: it also changes the desktop's wallpaper to the default Windows light or dark wallpaper.
+This repository contains PowerShell scripts for changing the dark-mode's and night-light's states of the Windows OS (and also of some applications).
+The main script is intended for use in a scheduled task. It sets the dark-mode's and night-light's states according to the given times.\
+Please note: The script that sets the dark-mode's state of the Windows OS changes the desktop's wallpaper to the default Windows light or dark wallpaper.
 
-It works by manipulating some strange registry values and calling a system call for changing the desktop's wallpaper.
-
-_Tested on Windows 11 (version 21H2, build 22000.708)._
+_Tested on Windows 11 (version 22H2, build 22621.963)._
---
+++ Set-DarkModeAndNightLightStates.ps1
@@ -0,0 +1,55 @@
+﻿[CmdletBinding()]
+param (
+    [Nullable[DateTime]] $darkModeStartTime,
+    [Nullable[DateTime]] $darkModeEndTime,
+    [Nullable[DateTime]] $nightLightStartTime,
+    [Nullable[DateTime]] $nightLightEndTime
+)
+
+
+function Get-IfShouldEnableDarkMode {
+    param (
+        [DateTime] $timeToCheckAgainst
+    )
+    if (($darkModeStartTime -ne $null) -and ($darkModeEndTime -ne $null)) {
+        $newDarkModeStartTime = Get-Date -Hour $darkModeStartTime.Hour -Minute $darkModeStartTime.Minute -Second $darkModeStartTime.Second
+        $newDarkModeEndTime = Get-Date -Hour $darkModeEndTime.Hour -Minute $darkModeEndTime.Minute -Second $darkModeEndTime.Second
+        if ($darkModeStartTime -le $darkModeEndTime) {
+            return (($timeToCheckAgainst -ge $newDarkModeStartTime) -and ($timeToCheckAgainst -lt $newDarkModeEndTime))
+        } else {
+            return (($timeToCheckAgainst -ge $newDarkModeStartTime) -or ($timeToCheckAgainst -lt $newDarkModeEndTime))
+        }
+    }
+    return $null
+}
+
+function Get-IfShouldEnableNightLight {
+    param (
+        [DateTime] $timeToCheckAgainst
+    )
+    if (($nightLightStartTime -ne $null) -and ($nightLightEndTime -ne $null)) {
+        $newNightLightStartTime = Get-Date -Hour $nightLightStartTime.Hour -Minute $nightLightStartTime.Minute -Second $nightLightStartTime.Second
+        $newNightLightEndTime = Get-Date -Hour $nightLightEndTime.Hour -Minute $nightLightEndTime.Minute -Second $nightLightEndTime.Second
+        if ($nightLightStartTime -le $nightLightEndTime) {
+            return (($timeToCheckAgainst -ge $newNightLightStartTime) -and ($timeToCheckAgainst -lt $newNightLightEndTime))
+        } else {
+            return (($timeToCheckAgainst -ge $newNightLightStartTime) -or ($timeToCheckAgainst -lt $newNightLightEndTime))
+        }
+    }
+    return $null
+}
+
+function Start-JobHere() {
+    param (
+        [scriptblock] $scriptBlock,
+        [object[]] $arguments
+    )
+    Start-Job -Init ([scriptblock]::Create("Set-Location `"$pwd`"")) -ScriptBlock $scriptBlock -ArgumentList $arguments
+}
+
+
+$currentTime = Get-Date
+$shouldEnableDarkMode = Get-IfShouldEnableDarkMode $currentTime
+$shouldEnableNightLight = Get-IfShouldEnableNightLight $currentTime
+# Add scripts below:
+Start-JobHere { scripts\Set-WindowsDarkModeAndNightLightStates.ps1 $args[0] $args[1] } ($shouldEnableDarkMode, $shouldEnableNightLight)
